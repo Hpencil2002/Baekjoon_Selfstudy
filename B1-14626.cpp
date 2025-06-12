dee9b581c4b70a648277cd5d804bbce7866ddd12
Hpencil2002 <mmin2002@naver.com>
2025-06-12 20:23:27 +0900

    2025년 6월 12일 문제풀이 - 14626번 오답 수정

--- B1-14626.cpp
+++ B1-14626.cpp
@@ -4,29 +4,26 @@ using namespace std;
 
 int main() {
     string ISBN;
-    int ans;
-    int tmp = 0;
+    int sum = 0;
 
     cin >> ISBN;
-    for (int i = 0; i < ISBN.length(); i++) {
-        if (ISBN[i] >= '0' && ISBN[i] <= '9') {
-            if (i % 2 == 0) {
-                tmp += ISBN[i] - '0';
+    
+    int loc = ISBN.find('*');
+    for (int i = 0; i < 10; i++) {
+        sum = 0;
+        ISBN[loc] = i + '0';
+
+        for (int j = 0; j < 13; j++) {
+            if (j % 2 == 1) {
+                sum += (ISBN[j] - '0') * 3;
+                continue;
             }
-            else {
-                tmp += (ISBN[i] - '0') * 3;
-            }
+            sum += (ISBN[j] - '0');
+        }
+
+        if (sum % 10 == 0) {
+            cout << ISBN[loc];
+            return 0;
         }
     }
-
-    if (tmp % 10 == 0) {
-        ans = 0;
-    }
-    else {
-        ans = 10 - tmp % 10;
-    }
-
-    cout << ans;
-
-    return 0;
 }
